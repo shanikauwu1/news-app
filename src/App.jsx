d554976ecddb30b1225e7c6ad01190c21b82280c
shanika Jayawardane <shanikauwu@gmail.com>
2024-10-09 10:53:04 -0600

    new changes on the path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { NavBar } from "./Components/NavBar";
+import { NavBar } from "./Components/Navbar";
 import { NewsBoard } from "./Components/Newsboard";
 
 function App() {
